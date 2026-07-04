4f24dc28821a667c92025931609edfe4f77bf486
theredenemy <1234corbin.1234ad@gmail.com>
2026-07-03 20:35:51 -0700

    modified:   python_ord_server/entrypoint.sh

--- python_ord_server/entrypoint.sh
+++ python_ord_server/entrypoint.sh
@@ -8,6 +8,30 @@ export INTERNAL_IP
 # Print current Python version
 python --version
 
+
+if [ ! -d .git ]; then
+    if [ -n "${GIT_ADDRESS}" ]; then
+        git clone "${GIT_ADDRESS}" .temp
+        mv .temp/* .temp/.* . 2>/dev/null
+        rm -rf .temp
+    else
+        exit 1
+    fi
+elif [[ "${AUTO_UPDATE}" == "1" ]]; then
+    git pull
+fi
+
+if [ ! -d "venv"]; then
+    python -m venv venv
+    ./venv/bin/pip install --upgrade pip
+fi
+
+source venv/bin/activate
+
+if [ -f "${REQUIREMENTS_FILE}"]; then
+    pip install --no-cache-dir -r "${REQUIREMENTS_FILE}"
+fi
+
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo -e $(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
